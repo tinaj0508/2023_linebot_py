1e5867cdc4760c9bc1993b98a5646887e51adc67
tinaj0508 <tinaj0508@gmail.com>
2023-10-08 14:20:34 +0800

    init

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 
+
+
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
